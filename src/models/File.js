1567388b2196094284c72e5bb9426b32d09847d5
Robert Liscano <robert.alr53@gmail.com>
2019-04-12 14:15:48 -0300

    adding all project

---
+++ src/models/File.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const File = new mongoose.Schema(
+  {
+    title: { 
+      type: String,
+      required: true,
+    },
+    path: {
+      type: String,
+      required: true,
+    },
+    files:[]
+  },
+  {
+    timestamps: true
+  }                                     
+);
+
+module.exports = mongoose.model("File", File);
